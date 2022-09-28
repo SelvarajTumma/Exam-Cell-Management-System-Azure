61188f4c7b6b69f32efcab981e7c5070b7d91d90
SelvarajTumma <selvarajtumma@gmail.com>
2022-09-28 13:46:03 +0530

    Added httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
